cdcd4bb3a7ba20f79b5647438ffad93bd6a6afdd
雨中漫步 <97043525+7kwl@users.noreply.github.com>
2026-08-12 14:47:02 +0800

    拆分

---
+++ WpfApp1/ECDSA/MainControl/EcdsaMainPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace WpfApp1.ECDSA.MainControl
+{
+    public partial class EcdsaMainPage : UserControl
+    {
+        public EcdsaMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
